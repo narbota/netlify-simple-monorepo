421263a04c24fe331e85133b4c5640b6fab8bde4
Andrea N <andrea@narbot.com>
2023-01-17 18:04:54 -0500

    Initial commit

--- bike-store/src/App.js
+++ bike-store/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to the Bike Store!</p>
+        <p>Welcome to Andrea's Bike Store!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
