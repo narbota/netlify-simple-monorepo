82bc7779ac33fa6ed0536951027648a6e0289473
Andrea N <andrea@narbot.com>
2023-01-17 20:26:30 -0500

    Remove 1 of the many constant

--- config/index.js
+++ config/index.js
@@ -1,3 +1,2 @@
 const this_should_be_a_constant = 'this should be a constant';
 const this_should_be_a_constant_too = 'this should be a constant too';
-const this_should_be_a_constant_too_too = 'this should be a constant too too';
