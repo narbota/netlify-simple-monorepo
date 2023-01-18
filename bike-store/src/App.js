eceebd97be6dacc9790a77e37c57accc1cd95774
Andrea N <andrea@narbot.com>
2023-01-17 21:49:40 -0500

    Update bike-store/app.js

--- bike-store/src/App.js
+++ bike-store/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Andrea's Bike Store v2!</p>
+        <p>Welcome to Andrea's Bike Store v3!</p>
        <p>Sit down and have a look!</p>
         </header>
     </div>
