bb3c69740fea8636abfa5ed29ad74a1e34f31e57
Andrea N <andrea@narbot.com>
2023-01-17 21:01:19 -0500

    Doing a change to v3

--- car-store/src/App.js
+++ car-store/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to the Andrea's Car Store v2!</p>
+        <p>Welcome to the Andrea's Car Store v3!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
