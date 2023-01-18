0c65271f0b3a856d8adcb7f746ceb4bfb3c90833
Andrea N <andrea@narbot.com>
2023-01-17 20:17:41 -0500

    Deploying car store

--- car-store/src/App.js
+++ car-store/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to the Andrea's Car!</p>
+        <p>Welcome to the Andrea's Car Store!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
