20181a4e542efc4b4c042d4ea779f83d3bfb2dac
Andrea N <andrea@narbot.com>
2023-01-19 10:50:40 -0500

    update app.js car store

--- apps/car-store/src/App.js
+++ apps/car-store/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to the Andrea's Car Store v5!</p>
+        <p>Welcome to the Andrea's Car Store for real!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
